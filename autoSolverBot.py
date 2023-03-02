db32dfac380d9369c75cb50d834fe9e1dd93ef1f
Dpakkk <pokharelbikash981@gmail.com>
2023-03-01 19:17:04 -0500

    regular expression updated

--- autoSolverBot.py
+++ autoSolverBot.py
@@ -10,7 +10,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import pyautogui
-#from time import sleep
 import re
 
 # browser code
